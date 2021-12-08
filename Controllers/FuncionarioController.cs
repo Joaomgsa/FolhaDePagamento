2b53ffa2090116f3c2aa648f6473052b3abb80f8
JOAO MARCOS <joaomgsa@gmail.com>
2021-12-08 12:31:25 -0300

    Primeiro commit

---
+++ Controllers/FuncionarioController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FolhaDePagamento.Models;
+
+namespace FolhaDePagamento.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        [Route("Funcionario/InserirPagamento")]
+        public IActionResult InserirPagamento()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [Route("Funcionario/FolhaPagamento")]
+        public IActionResult FolhaPagamento(Funcionario funcionario)
+        {
+            funcionario.DescontoInss = funcionario.Salario * 0.11;
+            funcionario.DescontoIrrf = funcionario.Salario * 0.07;
+
+
+            funcionario.Salario -= funcionario.DescontoInss;
+            
+            funcionario.Salario -= funcionario.DescontoIrrf;
+            
+            funcionario.Salario += funcionario.SalarioFamilia;
+            
+            funcionario.Salario += funcionario.Vr;
+
+            return View(funcionario);
+        }
+    }
+}
---
+++ Models/Funcionario.cs
@@ -0,0 +1,22 @@
+﻿namespace FolhaDePagamento.Models
+{
+    public class Funcionario
+    {
+
+        public String Nome { get; set;}
+        public String Rg { get; set; }
+
+        public string Cpf { get; set; }
+
+        public double Salario { get; set; }
+
+        public double Vr { get; set; }
+
+        public double SalarioFamilia { get; set; }
+
+        public double DescontoInss { get; set; }
+
+        public double DescontoIrrf { get; set; }
+
+    }
+}
